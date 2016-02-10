fe5d7ea79daedef38f05c1709238f1a3535c9328
Jamie Hewland <jhewland@gmail.com>
2016-02-10 15:09:33 +0200

    Add tests for default parameters

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -8,6 +8,40 @@ describe 'consular' do
       end
 
       it { is_expected.to compile }
+
+      describe 'with default parameters' do
+        it do
+          is_expected.to contain_class('consular::repo')
+            .with({
+              :manage => true,
+              :source => 'ppa-jerith',
+            })
+        end
+
+        it do
+          is_expected.to contain_package('python-consular')
+            .with_ensure('installed')
+            .that_requires('Class[consular::repo]')
+        end
+
+        it do
+          is_expected.to contain_file('/etc/init/consular.conf')
+            .with_content(/--host 127\.0\.0\.1/)
+            .with_content(/--port 7000/)
+            .with_content(/--consul http:\/\/127\.0\.0\.1:8500/)
+            .with_content(/--marathon http:\/\/127\.0\.0\.1:8080/)
+            .with_content(/--registration-id foo/)
+            .with_content(/--sync-interval 0/)
+            .with_content(/--no-purge/)
+            .that_requires('Package[python-consular]')
+        end
+
+        it do
+          is_expected.to contain_service('consular')
+            .with_ensure('running')
+            .that_subscribes_to('File[/etc/init/consular.conf]')
+        end
+      end
     end
   end
 end

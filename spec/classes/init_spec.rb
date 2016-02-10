f6ddf49fb35aaa912bf44e3cc2be4685c63da1ad
Jamie Hewland <jhewland@gmail.com>
2016-02-10 15:30:31 +0200

    Add more tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -42,6 +42,62 @@ describe 'consular' do
             .that_subscribes_to('File[/etc/init/consular.conf]')
         end
       end
+
+      describe 'when purge is true' do
+        let(:params) { { :purge => true } }
+        it do
+          is_expected.to contain_file('/etc/init/consular.conf')
+            .with_content(/--purge/)
+        end
+      end
+
+      describe 'when repo_manage is false' do
+        let(:params) { { :repo_manage => false } }
+        it do
+          is_expected.to contain_class('consular::repo')
+            .with_manage(false)
+        end
+      end
+
+      describe 'when repo_source is a custom value' do
+        let(:params) { { :repo_source => 'hello' } }
+        it do
+          is_expected.to raise_error(/not supported/)
+        end
+      end
+
+      describe 'when package_ensure is purged' do
+        let(:params) { { :package_ensure => 'purged' } }
+        it do
+          is_expected.to contain_package('python-consular')
+            .with_ensure('purged')
+        end
+      end
+
+      describe 'with extra options' do
+        let(:params) do
+          {
+            :extra_opts => {
+              'debug' => '',
+              'scheme' => 'https',
+            }
+          }
+        end
+        it do
+          is_expected.to contain_file('/etc/init/consular.conf')
+            .with_content(/--debug/)
+            .with_content(/--scheme https/)
+        end
+      end
+
+      describe 'with extra options that are also default options' do
+        let(:params) { { :extra_opts => { 'port' => '7001' } } }
+        it 'should override the default options' do
+          is_expected.to contain_file('/etc/init/consular.conf')
+            .with_content(/--port 7001/)
+            .without_content(/--port 7000/)
+        end
+      end
     end
   end
 end
